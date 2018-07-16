d1b738261dac92cd6aacfdb33cf69a9b908c1910
Frank Dedden <dev@dedden.net>
2018-07-16 16:13:25 -0400

    Added section 1.3 Identifiers

---
+++ src/Language/C99/AST.hs
@@ -0,0 +1,54 @@
+module Language.C99.AST where
+
+{- IDENTIFIERS -}
+{- 6.4.2.1 -}
+data Ident = IdentBase IdentNonDigit
+           | IdentConsNonDigit Ident IdentNonDigit
+           | IdentCons Ident Digit
+
+data IdentNonDigit = IdentNonDigit NonDigit
+                   | IdentNonDigitUniv UnivCharName
+                   {- Other implementation-defined characters -}
+
+data NonDigit = NDUnderscore
+              | NDa           | NDA
+              | NDb           | NDB
+              | NDc           | NDC
+              | NDd           | NDD
+              | NDe           | NDE
+              | NDf           | NDF
+              | NDg           | NDG
+              | NDh           | NDH
+              | NDi           | NDI
+              | NDj           | NDJ
+              | NDk           | NDK
+              | NDl           | NDL
+              | NDm           | NDM
+              | NDn           | NDN
+              | NDo           | NDO
+              | NDp           | NDP
+              | NDq           | NDQ
+              | NDr           | NDR
+              | NDs           | NDS
+              | NDt           | NDT
+              | NDu           | NDU
+              | NDv           | NDV
+              | NDw           | NDW
+              | NDx           | NDX
+              | NDy           | NDY
+              | NDz           | NDZ
+
+data Digit = DZero
+           | DOne
+           | DTwo
+           | DThree
+           | DFour
+           | DFive
+           | DSix
+           | DSeven
+           | DEight
+           | DNine
+
+
+-- TODO
+data UnivCharName
